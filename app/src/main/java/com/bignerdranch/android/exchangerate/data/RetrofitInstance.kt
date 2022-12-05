c9dda6190d5e06c50d002141ac444476ef7e83a0
AvenEnvoy <79922111892@yandex.ru>
2022-12-05 17:16:41 +0500

    project

--- app/src/main/java/com/bignerdranch/android/exchangerate/data/RetrofitInstance.kt
+++ app/src/main/java/com/bignerdranch/android/exchangerate/data/RetrofitInstance.kt
@@ -1,13 +1,13 @@
 package com.bignerdranch.android.exchangerate.data
 
 import retrofit2.Retrofit
-import retrofit2.converter.simplexml.SimpleXmlConverterFactory
+import retrofit2.converter.jaxb.JaxbConverterFactory
 
 object RetrofitInstance {
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl("https://cbr.ru/")
-            .addConverterFactory(SimpleXmlConverterFactory.create())
+            .addConverterFactory(JaxbConverterFactory.create())
             .build()
     }
     val api: ApiServices by lazy {
