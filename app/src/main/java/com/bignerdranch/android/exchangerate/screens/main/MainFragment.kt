3ac7ee5141229245599a157f6d412c99192e31ba
AvenEnvoy <79922111892@ya.ru>
2022-12-14 08:35:07 +0500

    project

--- app/src/main/java/com/bignerdranch/android/exchangerate/screens/main/MainFragment.kt
+++ app/src/main/java/com/bignerdranch/android/exchangerate/screens/main/MainFragment.kt
@@ -1,12 +1,11 @@
 package com.bignerdranch.android.exchangerate.screens.main
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
